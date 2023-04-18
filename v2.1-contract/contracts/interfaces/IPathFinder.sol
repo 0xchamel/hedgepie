1fd00a9e4844f83bf8f6b5d9b9fa0b221ddbab3c
0xchamel <mauroliu77@gmail.com>
2023-04-18 07:48:00 -0700

    feat: add comments for interfaces

--- v2.1-contract/contracts/interfaces/IPathFinder.sol
+++ v2.1-contract/contracts/interfaces/IPathFinder.sol
@@ -2,5 +2,11 @@
 pragma solidity ^0.8.4;
 
 interface IPathFinder {
+    /**
+     * @notice Get Path
+     * @param _router swap router address
+     * @param _inToken input token address
+     * @param _outToken output token address
+     */
     function getPaths(address _router, address _inToken, address _outToken) external view returns (address[] memory);
 }
