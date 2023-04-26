4c1bed6a2c58e45e410dd984f162dc73e6d61562
0xchamel <mauroliu77@gmail.com>
2023-04-26 23:49:38 +0900

    feat: update utils/index.ts

--- v2.1-contract/utils/index.ts
+++ v2.1-contract/utils/index.ts
@@ -32,162 +32,19 @@ async function verify({
 }
 
 const deployUsingFactory = async (factory, params, name) => {
-    let adapter;
-    if (params.length === 4) {
-        adapter = await factory.deploy(params[0], params[1], params[2], params[3]);
-    }
-
-    if (params.length === 5) {
-        adapter = await factory.deploy(params[0], params[1], params[2], params[3], params[4]);
-    }
-
-    if (params.length === 6) {
-        adapter = await factory.deploy(params[0], params[1], params[2], params[3], params[4], params[5]);
-    }
-
-    if (params.length === 7) {
-        adapter = await factory.deploy(params[0], params[1], params[2], params[3], params[4], params[5], params[6]);
-    }
-
-    if (params.length === 8) {
-        adapter = await factory.deploy(
-            params[0],
-            params[1],
-            params[2],
-            params[3],
-            params[4],
-            params[5],
-            params[6],
-            params[7]
-        );
-    }
-
-    if (params.length === 9) {
-        adapter = await factory.deploy(
-            params[0],
-            params[1],
-            params[2],
-            params[3],
-            params[4],
-            params[5],
-            params[6],
-            params[7],
-            params[8]
-        );
-    }
-
-    if (params.length === 10) {
-        adapter = await factory.deploy(
-            params[0],
-            params[1],
-            params[2],
-            params[3],
-            params[4],
-            params[5],
-            params[6],
-            params[7],
-            params[8],
-            params[9]
-        );
-    }
+    let adapter = await factory.deploy(...params);
 
     if (adapter) {
         await adapter.deployed();
         console.log(`${params[params.length - 2]} :`, adapter.address);
 
         // verify contracts
-        if (params.length === 4) {
-            await verify({
-                contractName: params[params.length - 2],
-                address: adapter.address,
-                constructorArguments: [params[0], params[1], params[2], params[3]],
-                contractPath: adapterPaths[name],
-            });
-        }
-
-        if (params.length === 5) {
-            await verify({
-                contractName: params[params.length - 2],
-                address: adapter.address,
-                constructorArguments: [params[0], params[1], params[2], params[3], params[4]],
-                contractPath: adapterPaths[name],
-            });
-        }
-
-        if (params.length === 6) {
-            await verify({
-                contractName: params[params.length - 2],
-                address: adapter.address,
-                constructorArguments: [params[0], params[1], params[2], params[3], params[4], params[5]],
-                contractPath: adapterPaths[name],
-            });
-        }
-
-        if (params.length === 7) {
-            await verify({
-                contractName: params[params.length - 2],
-                address: adapter.address,
-                constructorArguments: [params[0], params[1], params[2], params[3], params[4], params[5], params[6]],
-                contractPath: adapterPaths[name],
-            });
-        }
-
-        if (params.length === 8) {
-            await verify({
-                contractName: params[params.length - 2],
-                address: adapter.address,
-                constructorArguments: [
-                    params[0],
-                    params[1],
-                    params[2],
-                    params[3],
-                    params[4],
-                    params[5],
-                    params[6],
-                    params[7],
-                ],
-                contractPath: adapterPaths[name],
-            });
-        }
-
-        if (params.length === 9) {
-            await verify({
-                contractName: params[params.length - 2],
-                address: adapter.address,
-                constructorArguments: [
-                    params[0],
-                    params[1],
-                    params[2],
-                    params[3],
-                    params[4],
-                    params[5],
-                    params[6],
-                    params[7],
-                    params[8],
-                ],
-                contractPath: adapterPaths[name],
-            });
-        }
-
-        if (params.length === 10) {
-            await verify({
-                contractName: params[params.length - 2],
-                address: adapter.address,
-                constructorArguments: [
-                    params[0],
-                    params[1],
-                    params[2],
-                    params[3],
-                    params[4],
-                    params[5],
-                    params[6],
-                    params[7],
-                    params[8],
-                    params[9],
-                ],
-                contractPath: adapterPaths[name],
-            });
-        }
+        await verify({
+            contractName: params[params.length - 2],
+            address: adapter.address,
+            constructorArguments: [...params],
+            contractPath: adapterPaths[name],
+        });
     }
 
     return { name: params[params.length - 2], address: adapter.address };
