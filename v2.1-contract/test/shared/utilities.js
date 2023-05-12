0ca7d9254886b7ee49c0f39547796dcfe5657608
0xchamel <mauroliu77@gmail.com>
2023-05-13 00:32:20 +0900

    fix: fix test code with blocknumber

--- v2.1-contract/test/shared/utilities.js
+++ v2.1-contract/test/shared/utilities.js
@@ -23,7 +23,7 @@ async function forkBNBNetwork(blockNumber = -1) {
             {
                 forking: {
                     jsonRpcUrl: "https://rpc.ankr.com/bsc",
-                    blockNumber: blockNumber === -1 ? 27500000 : blockNumber,
+                    blockNumber: blockNumber === -1 ? 27000000 : blockNumber,
                 },
             },
         ],
