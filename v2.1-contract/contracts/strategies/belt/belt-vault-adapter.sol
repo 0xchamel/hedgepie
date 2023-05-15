94fd488f597d0c57b4d706354fb5013390ab04db
0xchamel <mauroliu77@gmail.com>
2023-05-16 00:35:05 +0900

    feat: update belt adapter

--- v2.1-contract/contracts/strategies/belt/belt-vault-adapter.sol
+++ v2.1-contract/contracts/strategies/belt/belt-vault-adapter.sol
@@ -4,17 +4,13 @@ pragma solidity ^0.8.4;
 import "../../libraries/HedgepieLibraryBsc.sol";
 
 interface IStrategy {
-    function deposit(uint256) external payable;
-
     function deposit(uint256, uint256) external;
+
+    function depositBNB(uint256) external payable;
 
     function withdraw(uint256, uint256) external;
 
     function withdrawBNB(uint256, uint256) external;
-
-    function balance() external view returns (uint256);
-
-    function totalSupply() external view returns (uint256);
 
     function getPricePerFullShare() external view returns (uint256);
 }
@@ -58,28 +54,33 @@ contract BeltVaultAdapterBsc is BaseAdapter {
         UserAdapterInfo storage userInfo = userAdapterInfos[_tokenId];
 
         // 1. get stakingToken
-        if (stakingToken != HedgepieLibraryBsc.WBNB)
-            amountOut = HedgepieLibraryBsc.swapOnRouter(msg.value, address(this), stakingToken, swapRouter);
+        bool isBNB = stakingToken == HedgepieLibraryBsc.WBNB;
+        amountOut = isBNB ? msg.value : amountOut = HedgepieLibraryBsc.swapOnRouter(
+            msg.value,
+            address(this),
+            stakingToken,
+            swapRouter
+        );
 
         // 2. deposit to vault
         uint256 repayAmt = IERC20(repayToken).balanceOf(address(this));
 
-        if (stakingToken == HedgepieLibraryBsc.WBNB) {
-            IStrategy(strategy).deposit{value: msg.value}(0);
+        if (isBNB) {
+            IStrategy(strategy).depositBNB{value: amountOut}(0);
         } else {
             IERC20(stakingToken).safeApprove(strategy, 0);
             IERC20(stakingToken).safeApprove(strategy, amountOut);
             IStrategy(strategy).deposit(amountOut, 0);
         }
-        repayAmt = IERC20(repayToken).balanceOf(address(this)) - repayAmt;
 
-        require(repayAmt != 0, "Failed to deposit");
+        unchecked {
+            repayAmt = IERC20(repayToken).balanceOf(address(this)) - repayAmt;
+            require(repayAmt != 0, "Failed to deposit");
+        }
 
         // 3. update user info
         userInfo.amount += repayAmt;
         userInfo.invested += amountOut;
-
-        return msg.value;
     }
 
     /**
@@ -96,31 +97,29 @@ contract BeltVaultAdapterBsc is BaseAdapter {
 
         UserAdapterInfo storage userInfo = userAdapterInfos[_tokenId];
 
-        bool isBNB = stakingToken == HedgepieLibraryBsc.WBNB;
-
         // 1. withdraw from Vault
-        uint256 lpOut = isBNB ? address(this).balance : IERC20(stakingToken).balanceOf(address(this));
+        bool isBNB = stakingToken == HedgepieLibraryBsc.WBNB;
+        uint256 tokenAmt = isBNB ? address(this).balance : IERC20(stakingToken).balanceOf(address(this));
         if (isBNB) {
             IStrategy(strategy).withdrawBNB(_amount, 0);
         } else {
             IStrategy(strategy).withdraw(_amount, 0);
         }
-        lpOut = (isBNB ? address(this).balance : IERC20(stakingToken).balanceOf(address(this))) - lpOut;
+        tokenAmt = (isBNB ? address(this).balance : IERC20(stakingToken).balanceOf(address(this))) - tokenAmt;
 
         // 2. swap withdrawn lp to bnb
-        if (!isBNB) amountOut = HedgepieLibraryBsc.swapForBnb(lpOut, address(this), stakingToken, swapRouter);
+        amountOut = isBNB ? tokenAmt : HedgepieLibraryBsc.swapForBnb(tokenAmt, address(this), stakingToken, swapRouter);
 
         // 3. update userInfo
-        userInfo.amount -= _amount;
+        unchecked {
+            userInfo.amount -= _amount;
 
-        if (lpOut >= userInfo.invested) userInfo.invested = 0;
-        else userInfo.invested -= lpOut;
+            if (tokenAmt >= userInfo.invested) userInfo.invested = 0;
+            else userInfo.invested -= tokenAmt;
+        }
 
         // 4. send withdrawn bnb to investor
-        if (amountOut != 0) {
-            (bool success, ) = payable(msg.sender).call{value: amountOut}("");
-            require(success, "Failed to send bnb");
-        }
+        if (amountOut != 0) _chargeFeeAndSendToInvestor(_tokenId, amountOut, 0);
     }
 
     /**
@@ -131,15 +130,13 @@ contract BeltVaultAdapterBsc is BaseAdapter {
     function claim(uint256 _tokenId) external payable override onlyInvestor returns (uint256 amountOut) {
         UserAdapterInfo storage userInfo = userAdapterInfos[_tokenId];
 
-        bool isBNB = stakingToken == HedgepieLibraryBsc.WBNB;
-
         // 1. check if reward is generated
         uint256 wantAmt = ((userInfo.amount * IStrategy(strategy).getPricePerFullShare()) / 1e18);
         uint256 wantShare = ((wantAmt > userInfo.invested ? wantAmt - userInfo.invested : 0) * 1e18) /
             IStrategy(strategy).getPricePerFullShare();
 
         // 2. if reward is not generated
-        if (wantAmt <= userInfo.invested) {
+        if (wantAmt <= userInfo.invested || wantShare == 0) {
             if (userInfo.rewardDebt1 == 0) return 0;
 
             amountOut = userInfo.rewardDebt1;
@@ -151,13 +148,18 @@ contract BeltVaultAdapterBsc is BaseAdapter {
         }
 
         // 3. withdraw reward from vault
-        wantAmt -= userInfo.invested;
-        uint256 lpOut = isBNB ? address(this).balance : IERC20(stakingToken).balanceOf(address(this));
-        IStrategy(strategy).withdraw(wantShare, 0);
-        lpOut = (isBNB ? address(this).balance : IERC20(stakingToken).balanceOf(address(this))) - lpOut;
+        bool isBNB = stakingToken == HedgepieLibraryBsc.WBNB;
+        amountOut = isBNB ? address(this).balance : IERC20(stakingToken).balanceOf(address(this));
+
+        if (isBNB) IStrategy(strategy).withdrawBNB(wantShare, 0);
+        else IStrategy(strategy).withdraw(wantShare, 0);
+
+        amountOut = (isBNB ? address(this).balance : IERC20(stakingToken).balanceOf(address(this))) - amountOut;
 
         // 4. swap reward to bnb
-        if (!isBNB) amountOut = HedgepieLibraryBsc.swapForBnb(lpOut, address(this), stakingToken, swapRouter);
+        if (!isBNB) amountOut = HedgepieLibraryBsc.swapForBnb(amountOut, address(this), stakingToken, swapRouter);
+
+        amountOut += userInfo.rewardDebt1;
 
         // 5. update user info
         userInfo.amount -= wantShare;
@@ -182,14 +184,14 @@ contract BeltVaultAdapterBsc is BaseAdapter {
 
         // 2. calc reward
         if (wantAmt != 0) {
-            if (stakingToken == HedgepieLibraryBsc.WBNB) reward += wantAmt;
+            if (stakingToken == HedgepieLibraryBsc.WBNB) reward = wantAmt;
             else {
                 address[] memory paths = IPathFinder(authority.pathFinder()).getPaths(
                     swapRouter,
                     stakingToken,
                     HedgepieLibraryBsc.WBNB
                 );
-                reward += IPancakeRouter(swapRouter).getAmountsOut(wantAmt, paths)[paths.length - 1];
+                reward = IPancakeRouter(swapRouter).getAmountsOut(wantAmt, paths)[paths.length - 1];
             }
         }
 
@@ -205,9 +207,8 @@ contract BeltVaultAdapterBsc is BaseAdapter {
         UserAdapterInfo storage userInfo = userAdapterInfos[_tokenId];
         if (userInfo.amount == 0) return 0;
 
-        bool isBNB = stakingToken == HedgepieLibraryBsc.WBNB;
-
         // 1. withdraw all from Vault
+        bool isBNB = stakingToken == HedgepieLibraryBsc.WBNB;
         amountOut = isBNB ? address(this).balance : IERC20(stakingToken).balanceOf(address(this));
         if (isBNB) {
             IStrategy(strategy).withdrawBNB(userInfo.amount, 0);
@@ -219,11 +220,13 @@ contract BeltVaultAdapterBsc is BaseAdapter {
         // 2. calc reward
         uint256 rewardPercent = 0;
         if (amountOut > userInfo.invested) {
-            rewardPercent = ((amountOut - userInfo.invested) * 1e12) / amountOut;
+            unchecked {
+                rewardPercent = ((amountOut - userInfo.invested) * 1e12) / amountOut;
+            }
         }
 
         // 3. swap withdrawn lp to bnb
-        amountOut = HedgepieLibraryBsc.swapForBnb(amountOut, address(this), stakingToken, swapRouter);
+        if (!isBNB) amountOut = HedgepieLibraryBsc.swapForBnb(amountOut, address(this), stakingToken, swapRouter);
 
         // 4. remove userInfo and stake pendingReward to rewardDebt1
         uint256 reward = (amountOut * rewardPercent) / 1e12;
@@ -232,8 +235,7 @@ contract BeltVaultAdapterBsc is BaseAdapter {
         userInfo.rewardDebt1 += reward;
 
         // 5. send withdrawn bnb to investor
-        (bool success, ) = payable(authority.hInvestor()).call{value: amountOut - reward}("");
-        require(success, "Failed to send bnb to investor");
+        if (amountOut != 0) _chargeFeeAndSendToInvestor(_tokenId, amountOut - reward, 0);
     }
 
     /**
@@ -247,28 +249,29 @@ contract BeltVaultAdapterBsc is BaseAdapter {
         UserAdapterInfo storage userInfo = userAdapterInfos[_tokenId];
 
         // 1. get stakingToken
-        if (stakingToken != HedgepieLibraryBsc.WBNB) {
-            amountOut = HedgepieLibraryBsc.swapOnRouter(msg.value, address(this), stakingToken, swapRouter);
-        }
+        bool isBNB = stakingToken == HedgepieLibraryBsc.WBNB;
+        amountOut = isBNB
+            ? msg.value
+            : HedgepieLibraryBsc.swapOnRouter(msg.value, address(this), stakingToken, swapRouter);
 
         // 2. deposit to vault
         uint256 repayAmt = IERC20(repayToken).balanceOf(address(this));
 
-        if (stakingToken == HedgepieLibraryBsc.WBNB) {
-            IStrategy(strategy).deposit{value: msg.value}(0);
+        if (isBNB) {
+            IStrategy(strategy).depositBNB{value: amountOut}(0);
         } else {
             IERC20(stakingToken).safeApprove(strategy, 0);
             IERC20(stakingToken).safeApprove(strategy, amountOut);
             IStrategy(strategy).deposit(amountOut, 0);
         }
-        repayAmt = IERC20(repayToken).balanceOf(address(this)) - repayAmt;
 
-        require(repayAmt != 0, "Failed to update funds");
+        unchecked {
+            repayAmt = IERC20(repayToken).balanceOf(address(this)) - repayAmt;
+            require(repayAmt != 0, "Failed to update funds");
 
-        // 3. update user info
-        userInfo.amount = repayAmt;
-        userInfo.invested = amountOut;
-
-        return msg.value;
+            // 3. update user info
+            userInfo.amount = repayAmt;
+            userInfo.invested = amountOut;
+        }
     }
 }
