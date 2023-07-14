95a6c16a5236cea7399f0c183ee86c82225589a1
0xchamel <mauroliu77@gmail.com>
2023-07-14 13:20:55 +1000

    feat: update integration test

--- v3.0-contract/test/integration/adapters/bnb/autofarm/autofarm-adapters.test.js
+++ v3.0-contract/test/integration/adapters/bnb/autofarm/autofarm-adapters.test.js
@@ -96,9 +96,29 @@ describe("AutoFarm Adapters Integration Test", function () {
         await this.adapterList
             .connect(this.adapterManager)
             .addAdapters([this.adapter[0].address, this.adapter[1].address]);
+        await this.adapterList
+            .connect(this.adapterManager)
+            .addInfo(
+                this.adapter[0].address,
+                ["AutoFarm::Vault::WBNB-CAKE"],
+                ["0x0895196562C7868C5Be92459FaE7f877ED450452"],
+                [619],
+                [0],
+                [0]
+            );
+        await this.adapterList
+            .connect(this.adapterManager)
+            .addInfo(
+                this.adapter[1].address,
+                ["PK::STAKE::SQUAD-ADAPTER"],
+                ["0x08C9d626a2F0CC1ed9BD07eBEdeF8929F45B83d3"],
+                [0],
+                [0],
+                [0]
+            );
 
         // mint ybnft
-        await mintNFT(this.ybNft, [this.adapter[0].address, this.adapter[1].address], this.performanceFee);
+        await mintNFT(this.ybNft, [this.adapter[0].address, this.adapter[1].address], [0, 0], this.performanceFee);
 
         this.checkAccRewardShare = async (tokenId) => {
             expect(
@@ -385,8 +405,8 @@ describe("AutoFarm Adapters Integration Test", function () {
     describe("Edit fund flow", function () {
         it("test possibility to set zero percent", async function () {
             await this.ybNft.connect(this.governor).updateAllocations(1, [
-                [0, this.adapter[0].address],
-                [10000, this.adapter[1].address],
+                [0, this.adapter[0].address, 0],
+                [10000, this.adapter[1].address, 0],
             ]);
         });
 
@@ -415,11 +435,11 @@ describe("AutoFarm Adapters Integration Test", function () {
         it("test pendingReward, invested amount ratio after allocation change", async function () {
             // Check reward increase after updateAllocation
             const allocation = [
-                [2000, this.adapter[0].address],
-                [8000, this.adapter[1].address],
+                [2000, this.adapter[0].address, 0],
+                [8000, this.adapter[1].address, 0],
             ];
-            const bTokenInfo1 = await this.adapter[0].userAdapterInfos(2);
-            const bTokenInfo2 = await this.adapter[1].userAdapterInfos(2);
+            const bTokenInfo1 = await this.adapter[0].userAdapterInfos(2, 0);
+            const bTokenInfo2 = await this.adapter[1].userAdapterInfos(2, 0);
             const bPending1 = await this.investor.pendingReward(1, this.user1.address);
             const bPending2 = await this.investor.pendingReward(2, this.user2.address);
             await this.ybNft.connect(this.governor).updateAllocations(2, allocation);
@@ -432,8 +452,8 @@ describe("AutoFarm Adapters Integration Test", function () {
             expect(aPending2[0]).gt(bPending2[0].mul(98).div(100));
 
             // check invested amount
-            const aTokenInfo1 = await this.adapter[0].userAdapterInfos(2);
-            const aTokenInfo2 = await this.adapter[1].userAdapterInfos(2);
+            const aTokenInfo1 = await this.adapter[0].userAdapterInfos(2, 0);
+            const aTokenInfo2 = await this.adapter[1].userAdapterInfos(2, 0);
             expect(BigNumber.from(bTokenInfo1.amount).div(50)).to.be.gt(
                 BigNumber.from(aTokenInfo1.amount).div(allocation[0][0]).mul(95).div(100)
             );
