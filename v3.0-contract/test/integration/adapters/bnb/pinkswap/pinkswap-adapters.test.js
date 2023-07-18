a21a1040c705a84e6a141585b5d333c1fa3f7d47
0xchamel <mauroliu77@gmail.com>
2023-07-18 19:53:07 +1000

    feat: update integration test

--- v3.0-contract/test/integration/adapters/bnb/pinkswap/pinkswap-adapters.test.js
+++ v3.0-contract/test/integration/adapters/bnb/pinkswap/pinkswap-adapters.test.js
@@ -78,9 +78,15 @@ describe("PinkSwap Adapters Integration Test", function () {
         await this.adapterList
             .connect(this.adapterManager)
             .addAdapters([this.adapter[0].address, this.adapter[1].address]);
+        await this.adapterList
+            .connect(this.adapterManager)
+            .addInfo(this.adapter[0].address, ["PinkSwap::Farm::BUSD-WBNB"], [strategy], [0], [0], [0]);
+        await this.adapterList
+            .connect(this.adapterManager)
+            .addInfo(this.adapter[1].address, ["PinkSwap::Farm::BUSD-WBNB"], [strategy], [1], [0], [0]);
 
         // mint ybnft
-        await mintNFT(this.ybNft, [this.adapter[0].address, this.adapter[1].address], this.performanceFee);
+        await mintNFT(this.ybNft, [this.adapter[0].address, this.adapter[1].address], [0, 0], this.performanceFee);
 
         this.checkAccRewardShare = async (tokenId) => {
             expect(
@@ -361,8 +367,8 @@ describe("PinkSwap Adapters Integration Test", function () {
     describe("Edit fund flow", function () {
         it("test possibility to set zero percent", async function () {
             await this.ybNft.connect(this.governor).updateAllocations(1, [
-                [0, this.adapter[0].address],
-                [10000, this.adapter[1].address],
+                [0, this.adapter[0].address, 0],
+                [10000, this.adapter[1].address, 0],
             ]);
         });
 
@@ -388,11 +394,11 @@ describe("PinkSwap Adapters Integration Test", function () {
         it("test pendingReward, invested amount ratio after allocation change", async function () {
             // Check reward increase after updateAllocation
             const allocation = [
-                [2000, this.adapter[0].address],
-                [8000, this.adapter[1].address],
+                [2000, this.adapter[0].address, 0],
+                [8000, this.adapter[1].address, 0],
             ];
-            const bTokenInfo1 = await this.adapter[0].userAdapterInfos(2);
-            const bTokenInfo2 = await this.adapter[1].userAdapterInfos(2);
+            const bTokenInfo1 = await this.adapter[0].userAdapterInfos(2, 0);
+            const bTokenInfo2 = await this.adapter[1].userAdapterInfos(2, 0);
             const bPending1 = await this.investor.pendingReward(1, this.user1.address);
             const bPending2 = await this.investor.pendingReward(2, this.user2.address);
             await this.ybNft.connect(this.governor).updateAllocations(2, allocation);
@@ -406,8 +412,8 @@ describe("PinkSwap Adapters Integration Test", function () {
                 expect(aPending2[1]).gt(bPending2[1].mul(90).div(100));
 
             // check invested amount
-            const aTokenInfo1 = await this.adapter[0].userAdapterInfos(2);
-            const aTokenInfo2 = await this.adapter[1].userAdapterInfos(2);
+            const aTokenInfo1 = await this.adapter[0].userAdapterInfos(2, 0);
+            const aTokenInfo2 = await this.adapter[1].userAdapterInfos(2, 0);
             expect(BigNumber.from(bTokenInfo1.amount).div(50)).to.be.gt(
                 BigNumber.from(aTokenInfo1.amount).div(allocation[0][0]).mul(95).div(100)
             );
